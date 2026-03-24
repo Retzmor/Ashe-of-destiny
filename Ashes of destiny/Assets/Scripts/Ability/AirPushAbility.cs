20e9b0e74d05519ddb7906fcb556aa69d8ec6314
GUZMAN_SENA <erwinsalas1278@gmail.com>
2026-03-24 11:06:40 -0500

    Version 0,20,1
    Arreglo de bugs

--- Ashes of destiny/Assets/Scripts/Ability/AirPushAbility.cs
+++ Ashes of destiny/Assets/Scripts/Ability/AirPushAbility.cs
@@ -1,40 +1,55 @@
 using UnityEngine;
-using UnityEngine.AI;
-using System.Collections;
-using Unity.VisualScripting;
+using System.Collections.Generic; 
 
 public class AirPushAbility : MonoBehaviour
 {
     public float force;
     public LayerMask enemyLayer;
-    [SerializeField] float rotationSpeed = 800f;
+    [SerializeField] Sprite airIcon;
     [SerializeField] float initialScale = 0.5f;
     [SerializeField] float maxScale = 3.0f;
     [SerializeField] float growthSpeed = 2.0f;
-    [SerializeField]float currentDetectionRadius;
+    [SerializeField] float currentDetectionRadius;
+
+    private List<Collider> enemiesAlreadyHit = new List<Collider>();
+
     void Start()
     {
         transform.localScale = Vector3.one * initialScale;
         Destroy(gameObject, 3);
     }
+
     private void Update()
     {
-        // Crecimiento del visual
+        // 1. Crecimiento visual
         if (transform.localScale.x < maxScale)
         {
             transform.localScale += Vector3.one * growthSpeed * Time.deltaTime;
         }
+
+        // 2. Detección de enemigos
         Collider[] enemiesHit = Physics.OverlapSphere(transform.position, currentDetectionRadius, enemyLayer);
+
         foreach (Collider enemy in enemiesHit)
         {
-            if (enemy.TryGetComponent(out EnemyKnockback knock))
+            if (!enemiesAlreadyHit.Contains(enemy))
             {
-                Vector3 pushDirection = (enemy.transform.position - transform.position).normalized;
-                pushDirection.y = 0.1f;
-                knock.Push(pushDirection, force);
+                if (enemy.TryGetComponent(out EnemyStatus status))
+                {
+                    status.ApplyElement("Air", airIcon);
+                }
+                if (enemy.TryGetComponent(out EnemyKnockback knock))
+                {
+                    Vector3 pushDirection = (enemy.transform.position - transform.position).normalized;
+                    pushDirection.y = 0.1f;
+                    knock.Push(pushDirection, force);
+                }
+
+                enemiesAlreadyHit.Add(enemy);
             }
         }
     }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;

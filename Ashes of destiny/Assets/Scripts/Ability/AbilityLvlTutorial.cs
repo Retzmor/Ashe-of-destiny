b6115418f992425924fc33ee1b691b333bc147c7
Retzmor <erwinsalas1278@gmail.com>
2026-03-26 23:57:04 -0500

    Parche

--- Ashes of destiny/Assets/Scripts/Ability/AbilityLvlTutorial.cs
+++ Ashes of destiny/Assets/Scripts/Ability/AbilityLvlTutorial.cs
@@ -5,6 +5,7 @@ public class AbilityLvlTutorial : MonoBehaviour
     [SerializeField] Ability air, fire;
     private void Start()
     {
+        // hola
         air.isInfinite = true;
         fire.isInfinite = true;
     }
